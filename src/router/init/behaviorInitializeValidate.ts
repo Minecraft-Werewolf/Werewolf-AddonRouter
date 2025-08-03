3e9e2982a188ffe01c4600ebd44b74bfc03a6133
shizuku86 <shizuku86710@gmail.com>
2025-08-04 03:54:12 +0900

    make behaviorInitializeValiate

---
+++ src/router/init/behaviorInitializeValidate.ts
@@ -0,0 +1,10 @@
+/**
+ * BehaviorInitializePendingに仮置きされたアドオンを、
+ * Registerで登録しても良いかどうかを検証するためのクラス
+ * 
+ * A class for validating whether addons temporarily stored in BehaviorInitializePending
+ * can be registered in Register.
+ */
+export class BehaviorInitializeValidate {
+
+}
